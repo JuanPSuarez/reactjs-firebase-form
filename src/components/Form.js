e98ee92ff4657620bd7852ad01be55f3098527ce
Juan Suarez <jp.juansuarez@gmail.com>
2023-01-07 17:43:09 -0300

    Added component Form, added CSS

---
+++ src/components/Form.js
@@ -0,0 +1,60 @@
+import React from "react";
+import "../index.css";
+
+const Form = ({ items }) => {
+  return (
+    <form>
+      {items.map((item) => {
+        return (
+          <div key={item.name}>
+            {item.type !== "submit" && (
+              <label htmlFor={item.name} className="label">
+                {item.label}
+              </label>
+            )}
+            {item.type === "text" ||
+            item.type === "email" ||
+            item.type === "date" ? (
+              <input
+                type={item.type}
+                name={item.name}
+                id={item.name}
+                required={item.required}
+                className="input"
+              />
+            ) : item.type === "select" ? (
+              <select
+                name={item.name}
+                id={item.name}
+                required={item.required}
+                className="select"
+              >
+                {item.options.map((option) => {
+                  return (
+                    <option key={option.value} value={option.value}>
+                      {option.label}
+                    </option>
+                  );
+                })}
+              </select>
+            ) : item.type === "checkbox" ? (
+              <input
+                type={item.type}
+                name={item.name}
+                id={item.name}
+                required={item.required}
+                className="input"
+              />
+            ) : (
+              <button type="submit" className="button">
+                {item.label}
+              </button>
+            )}
+          </div>
+        );
+      })}
+    </form>
+  );
+};
+
+export default Form;
