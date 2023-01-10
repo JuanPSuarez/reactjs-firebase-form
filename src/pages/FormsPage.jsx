34929552548bf09ab4b57ec186d81089fe52c4b6
Juan Suarez <jp.juansuarez@gmail.com>
2023-01-10 17:33:44 -0300

    Added alert box for form submited. Added checkbox support.

--- src/pages/FormsPage.jsx
+++ src/pages/FormsPage.jsx
@@ -1,20 +1,24 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Form from '../components/Form';
 import formData from '../assets/db.json';
 import { writeUserData } from '../components/Firebase';
+import { Alert, Grid } from '@mui/material';
 
 const App = () => {
-
+    const [show, setShow] = useState(false)
 useEffect(() => {
     const form = document.querySelector('form');
     form.addEventListener('submit', handleFormSubmit);
-
+    
     return () => {
         form.removeEventListener('submit', handleFormSubmit);
+        
     };
-}, []);
+}, );
 
 function handleFormSubmit(event) {
+
+    
     event.preventDefault();
     const form = event.target;
     const formData = new FormData(event.target);
@@ -25,8 +29,13 @@ function handleFormSubmit(event) {
     }
 
     writeUserData(data.email, data.full_name, data.birth_date, data.country_of_origin, data.terms_and_conditions);
-    window.alert('Form submitted successfully!');
-    form.reset()
+    // window.alert('Form submitted successfully!');    
+    form.reset();
+    setShow(!show) //alert
+    setTimeout(() => {
+        setShow(show)
+    }, 2000);
+    
 
     }
 
@@ -35,6 +44,11 @@ return (
     <div className="App">
         <div className="form-wrapper">
         <Form items={formData.items} />
+        {show ? <> 
+        <Grid container  alignItems="center" direction="column">
+        <Alert severity="success">Formulario enviado.</Alert> 
+        </Grid>
+        </>:null}
         <div>
         </div>
         </div>
